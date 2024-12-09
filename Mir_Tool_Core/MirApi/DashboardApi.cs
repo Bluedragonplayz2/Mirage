aa6f2ce9ee64a78e409eb1e4a5e013217ebfafe6
Bluedragonplayz2 <69000204+Bluedragonplayz2@users.noreply.github.com>
2024-12-09 17:48:26 +0800

    Added DashboardApi

---
+++ Mir_Tool_Core/MirApi/DashboardApi.cs
@@ -0,0 +1,113 @@
+﻿namespace Mir_Utilities;
+
+public class DashboardApi
+{
+    public async Task<List<DashboardApiSchema.GetDashboardSnapshot>?> GetDashboards(ApiCaller caller)
+    {
+        dynamic dashboardList = await caller.GetApi($"dashboards");
+        if (dashboardList.Count == 0)
+        {
+            return null;
+        }
+
+        List<DashboardApiSchema.GetDashboardSnapshot> dashboardListSnapshot = new List<DashboardApiSchema.GetDashboardSnapshot>();
+
+        foreach (var dashboard in dashboardList)
+        {
+            DashboardApiSchema.GetDashboardSnapshot dashboardSnapshot = new DashboardApiSchema.GetDashboardSnapshot();
+            dashboardSnapshot.Name = dashboard.name!;
+            dashboardSnapshot.Guid = dashboard.guid!;
+            dashboardListSnapshot.Add(dashboardSnapshot);
+        }
+        return dashboardListSnapshot;
+    }
+    public async Task<DashboardApiSchema.GetDashboardByGuidSnapshot> GetDashboardByGuid(ApiCaller caller, String guid)
+    {
+        dynamic dashboard = await caller.GetApi($"dashboards/{guid}");
+        DashboardApiSchema.GetDashboardByGuidSnapshot dashboardSnapshot = new DashboardApiSchema.GetDashboardByGuidSnapshot();
+        dashboardSnapshot.Name = dashboard.name!;
+        dashboardSnapshot.Guid = dashboard.guid!;
+        dashboardSnapshot.FleetDashboard = dashboard.fleet_dashboard!;
+        dashboardSnapshot.CreatedById = dashboard.created_by_id!;
+        return dashboardSnapshot;
+    }
+    public async Task<String> PostDashboard(ApiCaller caller, String guid, String name, String fleetDashboard)
+    {
+        dynamic dashboard = new
+        {
+            guid,
+            name,
+            fleet_dashboard = fleetDashboard,
+        };
+        dynamic response = await caller.PostApi("dashboards", dashboard);
+        return response.guid!;
+    }
+    public async Task<String> PutDashboard(ApiCaller caller, String guid, String name, String fleetDashboard)
+    {
+        dynamic dashboard = new
+        {
+            name,
+            fleet_dashboard = fleetDashboard,
+        };
+        dynamic response = await caller.PutApi($"dashboards/{guid}", dashboard);
+        return response.guid!;
+    }
+    public void DeleteDashboard(ApiCaller caller, String guid)
+    {
+        caller.DeleteApi($"dashboards/{guid}");
+    }
+    public async Task<List<DashboardApiSchema.GetWidgetsByDashboardSnapshot>?> GetWidgetsByDashboard(ApiCaller caller, String dashboardGuid)
+    {
+        dynamic widgetList = await caller.GetApi($"dashboards/{dashboardGuid}/widgets");
+        if (widgetList.Count == 0)
+        {
+            return null;
+        }
+
+        List<DashboardApiSchema.GetWidgetsByDashboardSnapshot> widgetListSnapshot = new List<DashboardApiSchema.GetWidgetsByDashboardSnapshot>();
+
+        foreach (var widget in widgetList)
+        {
+            DashboardApiSchema.GetWidgetsByDashboardSnapshot widgetSnapshot = new DashboardApiSchema.GetWidgetsByDashboardSnapshot();
+            widgetSnapshot.Guid = widget.guid!;
+            widgetListSnapshot.Add(widgetSnapshot);
+        }
+
+        return widgetListSnapshot;
+        
+    }
+    public async Task<DashboardApiSchema.GetWidgetByGuidSnapshot> GetWidgetByGuid(ApiCaller caller, String dashboardGuid, String widgetGuid)
+    {
+        dynamic widget = await caller.GetApi($"dashboards/{dashboardGuid}/widgets/{widgetGuid}");
+        DashboardApiSchema.GetWidgetByGuidSnapshot widgetSnapshot = new DashboardApiSchema.GetWidgetByGuidSnapshot();
+
+        widgetSnapshot.Guid = widget.guid!;
+        widgetSnapshot.DashboardId = widget.dashboard_id!;
+        widgetSnapshot.Settings = widget.settings!;
+        widgetSnapshot.CreatedById = widget.created_by_id!;
+        return widgetSnapshot;
+    }
+    public async Task<String> PostWidget(ApiCaller caller, String guid, String dashboardId, String settings)
+    {
+        dynamic widget = new
+        {
+            guid,
+            settings
+        };
+        dynamic response = await caller.PostApi($"dashboard/{dashboardId}/widgets", widget);
+        return response.guid!;
+    }
+    public async Task<String> PutWidget(ApiCaller caller, String guid, String dashboardId, String settings)
+    {
+        dynamic widget = new
+        {
+            settings
+        };
+        dynamic response = await caller.PutApi($"dashboard/{dashboardId}/widgets/{guid}", widget);
+        return response.guid!;
+    }
+    public void DeleteWidget(ApiCaller caller, String dashboardId, String guid)
+    {
+        caller.DeleteApi($"dashboard/{dashboardId}/widgets/{guid}");
+    }
+}
---
+++ Mir_Tool_Core/MirApi/DashboardApiSchema.cs
@@ -0,0 +1,32 @@
+﻿namespace Mir_Utilities;
+
+public class DashboardApiSchema
+{
+    public struct GetDashboardSnapshot
+    {
+        public String Name;
+        public String Guid;
+    }
+
+    public struct GetDashboardByGuidSnapshot
+    {
+        public String Guid;
+        public String Name;
+        public Boolean FleetDashboard;
+        public String CreatedById;
+    }
+
+    public struct GetWidgetsByDashboardSnapshot
+    {
+        public String Guid;
+
+    }
+    public struct GetWidgetByGuidSnapshot
+    {
+        public String Guid;
+        public String DashboardId;
+        public String Settings;
+        public String CreatedById;
+    }
+
+}
