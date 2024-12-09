bfd01be8de4c29045876f9fcfa42ff8dcde58f39
Bluedragonplayz2 <69000204+Bluedragonplayz2@users.noreply.github.com>
2024-12-09 19:50:05 +0800

    sync commit

--- Mir_Tool_Core/MirApi/MissionApiSchma.cs
+++ Mir_Tool_Core/MirApi/MissionApiSchma.cs
@@ -2,5 +2,22 @@
 
 public class MissionApiSchma
 {
-    
+    public struct GetMissionSnapshot
+    {
+        public String Name;
+        public String Guid;
+    }
+    public struct GetMissionByGuidSnapshot
+    {
+        public String Guid;
+        public String Name;
+        public String Description;
+        public String? SessionId;
+        public String? GroupId;
+        public Boolean Hidden;
+        public Boolean IsTemplate;
+        public String CreatedById;
+        public Boolean Valid;
+        public Boolean HasUserParameters;
+    }
 }
