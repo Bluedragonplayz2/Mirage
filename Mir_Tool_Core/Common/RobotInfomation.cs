b40a2aa55a1f8b19a702ae9c4f18dcab4f379e3b
Bluedragonplayz2 <69000204+Bluedragonplayz2@users.noreply.github.com>
2024-12-18 17:50:29 +0800

    Added Yaml config reader, started to create robot fleet config reader

---
+++ Mir_Tool_Core/Common/RobotInfomation.cs
@@ -0,0 +1,35 @@
+﻿namespace Mir_Utilities;
+
+public class RobotInfomation
+{
+    private struct RobotConfigSchema
+    {
+        public struct _robotDetails
+        {
+            public string ip;
+            public string port;
+            public string credential;
+            
+        }
+        public struct _credential
+        {
+            public string username;
+            public string password;
+        }
+
+        public List<Dictionary<string, _robotDetails>> robot;
+        public List<Dictionary<string, _credential >> credential;
+        
+    }
+    private readonly string _robotConfigPath = "./config/robotConfig.json";
+    public static List<RobotSchema.Robot> GetRobotsFromFleet(string fleetName)
+    {
+        
+        return new List<RobotSchema.Robot>();
+    }
+
+    public static RobotSchema GetRobot(string name)
+    {
+        
+    }
+}
--- Mir_Tool_Core/Common/YamlConfig.cs
+++ Mir_Tool_Core/Common/YamlConfig.cs
@@ -1,6 +1,41 @@
-﻿namespace Mir_Utilities;
+﻿using System.Text;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+
+namespace Mir_Utilities;
 
 public class YamlConfig
 {
+    private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
     
+    public static T? GetConfigFromFile<T>(string filePath) where T : struct
+    {
+        if (File.Exists(filePath))
+        {
+            string yamlString = ReadFile(filePath);
+            try
+            {
+                var deserializer = new DeserializerBuilder()
+                    .Build();
+                return deserializer.Deserialize<T>(yamlString);
+            }
+            catch (YamlException yamlEx)
+            {
+                logger.Error($"Failed to deserialize yaml file: {filePath}, error: {yamlEx.Message}");
+                return null;
+            }
+        }
+        logger.Info($"File not found: {filePath}");
+        return null;
+    }
+
+    private static string ReadFile(string path)
+    {
+        string readContents;
+        using (StreamReader streamReader = new StreamReader(path, Encoding.UTF8))
+        {
+            readContents = streamReader.ReadToEnd();
+        }
+        return readContents;
+    }
 }
