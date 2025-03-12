67d10f524bf18db91e027b64115631c0c3fb9a7f
Bluedragonplayz2 <69000204+Bluedragonplayz2@users.noreply.github.com>
2025-03-12 19:18:40 +0800

    Modify MapSchema to better suit conventions and be more efficient

--- Mir_Tool_Core/Common/MapSchema.cs
+++ Mir_Tool_Core/Common/MapSchema.cs
@@ -1,64 +1,93 @@
-﻿namespace Mir_Utilities.Common;
+﻿
+using System.Drawing;
+using System.Runtime.InteropServices;
 
-public struct Map
+namespace Mir_Utilities.Common;
+
+public class Map
 {
-    public string Guid;
-    public string Name;
-    public string SiteId;
-    public float OriginX;
-    public float OriginY;
-    public float Resolution;
-    public float OriginTheta;
-    public List<Position>? Positions;
-    public List<Zone>? Zones;
-    public List<PathGuide>? PathGuides;
-    public string BaseMap;
-
-    public struct Position
+    public string Guid { get; private set; } = "";
+    public string Name { get; private set; } = "";
+    public string SiteId { get; private set; } = "";
+    public float OriginX { get; private set; } 
+    public float OriginY { get; private set; }
+    public float Resolution { get; private set; }
+    public float OriginTheta { get; private set; }
+    public Position[] Positions { get; set; } = [];
+    public Zone[] Zones { get; set; } = [];
+    public PathGuide[] PathGuides { get; set; } = [];
+    public byte[] BaseMap { get; private set; } = [];
+    public class Position
     {
-        public string Guid;
-        public string Name;
-        public float PosX;
-        public float PosY;
-        public float Orientation;
-        public int TypeId;
-        public List<Position>? HelperPositions;
+        public string Guid { get; private set; } = "";
+        public string Name { get; private set; } = "";
+        public float PosX { get; private set; }
+        public float PosY { get; private set; }
+        public float Orientation { get; private set; }
+        public int TypeId { get; private set; }
+        public Position[] HelperPositions { get; set; } = [];
     }
 
-    public struct Zone
+    public class Zone
     {
-        public string Guid;
-        public string Name;
-        public string ShapeType;
-        public int TypeId;
-        public float StrokeWidth;
-        public float Direction;
-        public List<Coordinates> Polygon;
+        public string Guid { get; private set; } = "";
+        public string Name { get; private set; } = "";
+        public string ShapeType { get; private set; } = "";
+        public int TypeId { get; private set; }
+        public float StrokeWidth { get; private set; }
+        public float Direction { get; private set; }
+        public Coordinates[] Polygon { get; private set; } = [];
 
-        public struct Coordinates
+        public class Coordinates
         {
-            public float X;
-            public float Y;
+            public float X { get; private set; }
+            public float Y { get; private set; }
         }
-        public dynamic Actions;
+
+        public object? Actions { get; private set; } = null;
 
     }
 
-    public struct PathGuide
+    public class PathGuide
     {
-        public string Guid;
-        public string Name;
-        public List<PathPosition> PathPositions;
+        public string Guid { get; set; } = "";
+        public string Name { get; set; } = "";
+        public PathPosition[] PathPositions { get; set; } = [];
 
-        public struct PathPosition
+        public class PathPosition
         {
-            public string Guid;
-            public string PositionGuid;
-            public int Priority;
-            public string PositionType;
+            public string Guid { get; set; } = "";
+            public string PositionGuid { get; set; } = "";
+            public int Priority { get; set; }
+            public string PositionType { get; set; } = "";
         }
     }
-    
+    public void SetBasicMapData(string guid, string name, string siteId, float originX, float originY, float resolution, float originTheta, string baseMap)
+    {
+        Guid = guid;
+        Name = name;
+        SiteId = siteId;
+        OriginX = originX;
+        OriginY = originY;
+        Resolution = resolution;
+        OriginTheta = originTheta;
+        BaseMap = Convert.FromBase64String(baseMap);
+    }
+
+
+
+    public string GetMapBase64()
+    {
+        return Convert.ToBase64String(BaseMap);
+    }
+    public Bitmap GetMapBitmap()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException("System.Drawing.Bitmap is only supported on Windows!");
+        using MemoryStream ms = new MemoryStream(BaseMap);
+        return new Bitmap(ms);
+    }
+
 }
 
 
